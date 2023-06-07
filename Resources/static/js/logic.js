5cb6eb8ad22b60405465d9df0139822fdaa60996
GigiRibeiro <122299838+GigiRibeiro@users.noreply.github.com>
2023-06-07 15:26:55 -0400

    Update logic.js

---
+++ Resources/static/js/logic.js
@@ -0,0 +1,29 @@
+https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson
+
+// Part 1
+// Import and visualize the data by doing the following:
+
+// Using Leaflet, create a map that plots all the earthquakes from your dataset based on their longitude and latitude.
+
+// Your data markers should reflect the magnitude of the earthquake by their size and the depth of the earthquake by color. Earthquakes with higher magnitudes should appear larger, and earthquakes with greater depth should appear darker in color.
+
+// Hint: The depth of the earth can be found as the third coordinate for each earthquake.
+
+// Include popups that provide additional information about the earthquake when its associated marker is clicked.
+
+// Create a legend that will provide context for your map data.
+
+// Your visualization should look something like the preceding map.
+
+
+// Parte 2 BOnus
+
+// Perform the following tasks:
+
+// Plot the tectonic plates dataset on the map in addition to the earthquakes.
+
+// Add other base maps to choose from.
+
+// Put each dataset into separate overlays that can be turned on and off independently.
+
+// Add layer controls to your map.
